d95dca276a3a69241742a1387a2158ff0db25162
mbielstein2 <mbielstein2@huskers.unl.edu>
2021-04-21 16:02:21 -0500

    Add static sql data retrieving methods in HD app

--- Health_Department_App/health_department.py
+++ Health_Department_App/health_department.py
@@ -13,12 +13,12 @@ from sqlalchemy.orm import sessionmaker
 from database import *
 import mysql.connector
 
+
 class HomeScreen(Screen):
     pass
 
+
 class Health_departmentApp(MDApp):
-
-
     def build(self):
         self.theme_cls.primary_palette = "Blue"
 
@@ -29,6 +29,62 @@ class Health_departmentApp(MDApp):
         return sm
 
 
+def get_sql_data(table_name, column_name):
+    database = mysql.connector.connect(host=host, database=database_name, user=user, password=password)
+    finder = database.cursor(buffered=True)
+    if column_name is None:
+        finder.execute(f'SELECT * FROM {table_name}')
+        return finder.fetchall()
+    else:
+        finder.execute(f'SELECT {column_name} FROM {table_name}')
+        list_of_columns = []
+        for element in finder.fetchall():
+            list_of_columns.append(element[0])
+        return list_of_columns
+
+
+# returns a single element from table
+def get_specific_sql_data(table_name, column, identifier_column, oracle):
+    database = mysql.connector.connect(host=host, database=database_name, user=user, password=password)
+    finder = database.cursor(buffered=True)
+    finder.execute(f'select {column} from {table_name} where {identifier_column} = \"{oracle}\"')
+    result = finder.fetchall()
+    return_list = []
+    for item in result:
+        return_list.append(item[0])
+
+    if len(return_list) is not 0:
+        return return_list
+    else:
+        return []
+
+
+def load_credentials_file():
+    try:
+        with open('credentials.json', 'r') as credentials_file:
+            credentials = json.load(credentials_file)
+            global host
+            global database_name
+            global user
+            global password
+            host = credentials['host']
+            database_name = credentials['database']
+            user = credentials['username']
+            password = credentials['password']
+    except FileNotFoundError:
+        # Replace this with error prompt in app
+        print('Database connection failed!')
+        print('credentials.json not found')
+        exit(1)
+
+
+# Global variables:
+global host
+global database_name
+global user
+global password
+
 if __name__ == '__main__':
     app = Health_departmentApp()
     app.run()
+    load_credentials_file()
